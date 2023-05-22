382ec8dd074cdb1388113aa6106dc8de36490811
AgastyaJXD7 <agastyaj.87075@gmail.com>
2023-05-22 11:13:51 +0530

    first commit

---
+++ client/src/pages/agent-profile.tsx
@@ -0,0 +1,18 @@
+import {useOne} from '@pankod/refine-core'
+import { useParams } from '@pankod/refine-react-router-v6'
+import { Profile } from 'components'
+
+const AgentProfile = () => {
+  const {id} = useParams()
+  const {data, isLoading, isError} = useOne({resource: "users", id: id as string,})
+
+  const myProfile = data?.data ?? []
+  if(isLoading) return <div>loading...</div>
+  if(isError) return <div>error...</div>
+
+  return (
+    <Profile type="Agent" name={myProfile.name} email={myProfile.email} properties={myProfile.allProperties} avatar={myProfile.avatar}/>
+  )
+}
+
+export default AgentProfile
