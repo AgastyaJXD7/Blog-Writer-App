6f2529c616d533159b995c685d8736c1e023a3b8
AgastyaJXD7 <agastyaj.87075@gmail.com>
2023-05-22 20:33:05 +0530

    Replacing Localhost 8080 with server URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -63,7 +63,7 @@ function App() {
       const profileObj = credential ? parseJwt(credential) : null;
 
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://blog-app-h6vm.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -139,7 +139,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://blog-app-h6vm.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
